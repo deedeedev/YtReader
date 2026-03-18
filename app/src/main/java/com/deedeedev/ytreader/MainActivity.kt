1a153ab0a288d7cac0de9a74a4b10aadd50073ae
Davide <davide.sintini@gmail.com>
2026-03-18 12:16:07 +0100

    adds splash screen

--- app/src/main/java/com/deedeedev/ytreader/MainActivity.kt
+++ app/src/main/java/com/deedeedev/ytreader/MainActivity.kt
@@ -6,9 +6,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.deedeedev.ytreader.ui.MainScreen
@@ -20,6 +20,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_YtReader)
         enableEdgeToEdge()
         latestIntent = intent
         val appContainer = (application as YtReaderApplication).container
